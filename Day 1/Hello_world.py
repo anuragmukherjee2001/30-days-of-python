7a78e3aa90977443fbaa60a720a271c5702b6aaa
Anurag <anuragmukherjee2001@gmail.com>
2020-12-07 13:41:08 +0530

    Completed the day 1 on 7/12/2020

---
+++ Day 1/Hello_world.py
@@ -0,0 +1,28 @@
+# The operands are 3 and 4
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+name = "Anurag"
+country = "India"
+comment = "I am enjoying 30 days of python"
+
+# printing the strings
+
+print(name)
+print(country)
+print(comment)
+
+# Checking the datatypes
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(['Anurag', 'Python', 'India']))
+print(type("Anurag"))
+print(type("India"))
